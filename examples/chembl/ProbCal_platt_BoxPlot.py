1c2f3d2cec0dce33e9ffefe57e88e786ddb801a1
hannahrosafriesacher <rosafriesacher@live.at>
2022-01-10 12:11:03 +0100

    Change and complete comments

--- examples/chembl/ProbCal_platt_BoxPlot.py
+++ examples/chembl/ProbCal_platt_BoxPlot.py
@@ -24,10 +24,8 @@ parser.add_argument("--targetID", help="TargetID", type=int, required=True)
 args = parser.parse_args()
 conf = sc.load_results(args.conf, two_heads=True)["conf"]
 TargetID=args.targetID
-
 y_class = sc.load_sparse(args.y_class)
 y_hat_platt=sc.load_sparse(args.y_hat_platt)
-
 
 #select correct fold for y_class
 if args.folding is not None:
@@ -45,18 +43,18 @@ y_class_TargetID=y_class[:, TargetID]
 y_class_selected=y_class_TargetID[np.nonzero(y_class_TargetID)]
 y_class_selected=y_class_selected.A.flatten()
 
-#Obtain positive count for compounds with predicted values betwen 0.0-0.1, 0.1-0.2, ...
+#Split array according to condition
 def split(arr, cond):
     return arr[cond]
+#Calculate positive 
 def posRatio(arr):
     return (arr==1).sum()/arr.shape[0]
-    
-#split into positives and negatives in each class in the _y_class file
+#split into positives and negatives in each class in the y_class file
 def selectPosNeg(arr):
     pos=np.count_nonzero(arr==1)
     neg=np.count_nonzero(arr==-1)
     return pos, neg
-
+#count number of positives/negatives
 def selectPos(arr):
     pos=np.count_nonzero(arr==1)
     return pos
@@ -64,6 +62,7 @@ def selectNeg(arr):
     neg=np.count_nonzero(arr==-1)
     return neg
 
+#obtain postive Ratio and negative/positive count for each class of predicted probablities
 clas=[]
 acc=[]
 NumPos=[]
@@ -73,12 +72,9 @@ j=0
 k=0
 
 for j in range(10):
-    #print(split(y_hat_platt, np.logical_and(y_hat_platt>=values[j], y_hat_platt<values[j+1])))
     clas.append(split(y_class_selected,np.logical_and(y_hat_platt>=values[j], y_hat_platt<values[j+1])))
     j+=1
     values[j]
-
-
 for k in range(10):
     acc.append(posRatio(clas[k]))
     NumPos.append(selectPos(clas[k]))
@@ -122,8 +118,7 @@ for i in range(10):
 #Prepare for Plotting
 X_axis_bar= ['0.0-0.1', '0.1-0.2', '0.2-0.3', '0.3-0.4', '0.4-0.5','0.5-0.6', '0.6-0.7', '0.7-0.8', '0.8-0-9', '0.9-1.0'] 
  
-#X_axis_hist=np.array([0.0, 0.1,0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0])
-#print('Plotted positive counts:', acc)
+#Plot Boxplot with a Whiskers Blot for each 'probability class'
 fig, axs=plt.subplots(2, 1, figsize=(9,9))
 
 axs[0].bxp(stats_box, showfliers=False, meanline=True)
@@ -133,6 +128,7 @@ axs[0].set_xlabel('predicted activity')
 axs[0].set_ylabel('positive ratio')
 axs[0].axline([1,0.05], [10,0.95], color='r', linestyle='--')
 
+#Plot number of compounds of eac 'class'
 heights, bins= np.histogram(np.transpose(y_hat_platt))
 axs[1].bar(bins[:-1], heights/heights.sum(), align='center', tick_label=X_axis_bar, width=bins[1]-bins[0])
 axs[1].set_title('Counts', fontsize='x-large')
@@ -141,5 +137,7 @@ axs[1].set_ylabel('relative frequency')
 
 fig.suptitle('Target-ID:'+ str(TargetID) + '\n Total Number of Bioactivities:' + str(y_hat_platt.shape[0]),fontsize='xx-large' )
 fig.tight_layout()
+
+#save figure
 plt.savefig('./ProbCal_plots/BoxPlot_Count and_PositiveRate_TargetID_plattScaling'+str(TargetID)+'_'+str(date)+'.pdf')
 
